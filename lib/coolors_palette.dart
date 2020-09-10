40c2ae4761c8e15987eab04f37a896a9a51514cc
Mert Gül <ibrahimmertgul@gmail.com>
2020-09-10 11:06:38 +0300

    Class created

--- lib/coolors_palette.dart
+++ lib/coolors_palette.dart
@@ -1,7 +1,33 @@
 library coolors_palette;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class CoolorsPalette {
+  var colors;
+  var colorsList;
+  List<Color> palette = List<Color>();
+
+  CoolorsPalette(String url) {
+    var temp = url.split("/");
+    colors = temp[temp.length - 1];
+    colorsList = colors.split("-");
+    createPalette();
+  }
+
+  Color fromHex(String hexString) {
+    final buffer = StringBuffer();
+    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
+    buffer.write(hexString.replaceFirst('#', ''));
+    return Color(int.parse(buffer.toString(), radix: 16));
+  }
+
+  String print() {
+    return colors;
+  }
+
+  void createPalette() {
+    for (var i = 0; i < colorsList.length; i++) {
+      palette.add(fromHex(colorsList[i]));
+    }
+  }
 }
