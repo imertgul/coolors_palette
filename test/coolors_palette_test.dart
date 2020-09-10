f72ecd53f44fcfb531e6feee53b27db54aecd65e
Mert Gül <ibrahimmertgul@gmail.com>
2020-09-10 11:06:47 +0300

    Worked perfectly

--- test/coolors_palette_test.dart
+++ test/coolors_palette_test.dart
@@ -1,13 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:coolors_palette/coolors_palette.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+    final renklerim = CoolorsPalette(
+        "https://coolors.co/055f3d-066943-07744a-313131-383c3f-34454d-d2433b-b13126-97190b-d10a10");
+    print(renklerim.print());
+    for (var i = 0; i < renklerim.palette.length; i++) {
+      Color temp = renklerim.palette[i];
+      print(temp);
+    }
   });
 }
