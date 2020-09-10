bf6aa491907a02e13c007c5a78825df63f725a1f
Mert Gül <ibrahimmertgul@gmail.com>
2020-09-10 11:40:49 +0300

    Update coolors_palette_test.dart

--- test/coolors_palette_test.dart
+++ test/coolors_palette_test.dart
@@ -3,13 +3,12 @@ import 'package:coolors_palette/coolors_palette.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final renklerim = CoolorsPalette(
+  test('Adds colors to palette list nad prints the colors', () {
+    final myColors = CoolorsPalette(
         "https://coolors.co/055f3d-066943-07744a-313131-383c3f-34454d-d2433b-b13126-97190b-d10a10");
-    print(renklerim.print());
-    for (var i = 0; i < renklerim.palette.length; i++) {
-      Color temp = renklerim.palette[i];
-      print(temp);
+    print(myColors.print());
+    for (var i = 0; i < myColors.palette.length; i++) {
+      print(myColors.palette[i]);
     }
   });
 }
